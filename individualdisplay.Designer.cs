f7e9090f7823485cb3d0b4d18594118e5c1049b7
sanskarbhusal <101934318+sanskarbhusal@users.noreply.github.com>
2024-12-07 20:36:56 +0545

    Add files via upload

---
+++ individualdisplay.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace CosmosProject
+{
+    partial class individualdisplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(individualdisplay));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.s2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.s3 = new System.Windows.Forms.Label();
+            this.pName = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.s1 = new System.Windows.Forms.Label();
+            this.semester = new System.Windows.Forms.Label();
+            this.sName = new System.Windows.Forms.Label();
+            this.program = new System.Windows.Forms.Label();
+            this.batch = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.s4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(212, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registration ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(209, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(516, 45);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Search Individual Project Details";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(449, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Supervisor Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(449, 292);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Batch";
+            // 
+            // s2
+            // 
+            this.s2.AutoSize = true;
+            this.s2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s2.Location = new System.Drawing.Point(276, 368);
+            this.s2.Name = "s2";
+            this.s2.Size = new System.Drawing.Size(82, 23);
+            this.s2.TabIndex = 4;
+            this.s2.Text = "________";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(449, 363);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 29);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Semester";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(449, 441);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 29);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Program";
+            // 
+            // s3
+            // 
+            this.s3.AutoSize = true;
+            this.s3.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s3.Location = new System.Drawing.Point(276, 446);
+            this.s3.Name = "s3";
+            this.s3.Size = new System.Drawing.Size(82, 23);
+            this.s3.TabIndex = 7;
+            this.s3.Text = "________";
+            // 
+            // pName
+            // 
+            this.pName.AutoSize = true;
+            this.pName.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pName.Location = new System.Drawing.Point(276, 227);
+            this.pName.Name = "pName";
+            this.pName.Size = new System.Drawing.Size(82, 23);
+            this.pName.TabIndex = 8;
+            this.pName.Text = "________";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(96, 222);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(149, 29);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Project Name";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(96, 292);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(103, 29);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Student1";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(96, 363);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(103, 29);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Student2";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(96, 441);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(103, 29);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "Student3";
+            // 
+            // s1
+            // 
+            this.s1.AutoSize = true;
+            this.s1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s1.Location = new System.Drawing.Point(276, 297);
+            this.s1.Name = "s1";
+            this.s1.Size = new System.Drawing.Size(82, 23);
+            this.s1.TabIndex = 13;
+            this.s1.Text = "________";
+            // 
+            // semester
+            // 
+            this.semester.AutoSize = true;
+            this.semester.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.semester.Location = new System.Drawing.Point(656, 368);
+            this.semester.Name = "semester";
+            this.semester.Size = new System.Drawing.Size(82, 23);
+            this.semester.TabIndex = 14;
+            this.semester.Text = "________";
+            // 
+            // sName
+            // 
+            this.sName.AutoSize = true;
+            this.sName.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sName.Location = new System.Drawing.Point(656, 228);
+            this.sName.Name = "sName";
+            this.sName.Size = new System.Drawing.Size(82, 23);
+            this.sName.TabIndex = 15;
+            this.sName.Text = "________";
+            // 
+            // program
+            // 
+            this.program.AutoSize = true;
+            this.program.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.program.Location = new System.Drawing.Point(656, 446);
+            this.program.Name = "program";
+            this.program.Size = new System.Drawing.Size(82, 23);
+            this.program.TabIndex = 16;
+            this.program.Text = "________";
+            // 
+            // batch
+            // 
+            this.batch.AutoSize = true;
+            this.batch.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.batch.Location = new System.Drawing.Point(656, 297);
+            this.batch.Name = "batch";
+            this.batch.Size = new System.Drawing.Size(82, 23);
+            this.batch.TabIndex = 17;
+            this.batch.Text = "________";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(156, 144);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(388, 124);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.TabIndex = 19;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(602, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 36);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(96, 508);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 29);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Student4";
+            // 
+            // s4
+            // 
+            this.s4.AutoSize = true;
+            this.s4.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s4.Location = new System.Drawing.Point(276, 513);
+            this.s4.Name = "s4";
+            this.s4.Size = new System.Drawing.Size(82, 23);
+            this.s4.TabIndex = 21;
+            this.s4.Text = "________";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Green;
+            this.button2.Location = new System.Drawing.Point(458, 513);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 36);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // individualdisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 593);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.s4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.batch);
+            this.Controls.Add(this.program);
+            this.Controls.Add(this.sName);
+            this.Controls.Add(this.semester);
+            this.Controls.Add(this.s1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.pName);
+            this.Controls.Add(this.s3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.s2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "individualdisplay";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IndividualDisplay";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label s2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label s3;
+        private System.Windows.Forms.Label pName;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label s1;
+        private System.Windows.Forms.Label semester;
+        private System.Windows.Forms.Label sName;
+        private System.Windows.Forms.Label program;
+        private System.Windows.Forms.Label batch;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label s4;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ individualdisplay.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CosmosProject
+{
+    public partial class individualdisplay : Form
+    {
+        public individualdisplay()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\LEVEL51PC\\OneDrive\\Desktop\\dot net\\c#\\dil bahadur\\lab2\\CosmosProject\\CosmosProject\\Database1.mdf\";Integrated Security=True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select * from tbl where id = '"+textBox1.Text+"'";
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            ad.Fill(ds);
+            if (ds.Tables[0].Rows.Count != 0)
+            {
+                pName.Text = ds.Tables[0].Rows[0][1].ToString();
+                s1.Text = ds.Tables[0].Rows[0][2].ToString();
+                s2.Text = ds.Tables[0].Rows[0][3].ToString();
+                s3.Text = ds.Tables[0].Rows[0][4].ToString();
+                s4.Text = ds.Tables[0].Rows[0][5].ToString();
+                sName.Text = ds.Tables[0].Rows[0][6].ToString();
+                batch.Text = ds.Tables[0].Rows[0][7].ToString();
+                semester.Text = ds.Tables[0].Rows[0][8].ToString();
+                program.Text = ds.Tables[0].Rows[0][9].ToString();
+            }
+            else
+            {
+                MessageBox.Show("No Record Found","No Match",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pName.Text = null;
+            s1.Text = null;
+            s2.Text = null;
+            s3.Text = null;
+            s4.Text = null;
+            sName.Text = null;
+            batch.Text = null;
+            semester.Text = null;
+            program.Text = null;
+            textBox1.Text = null;
+        }
+    }
+}
